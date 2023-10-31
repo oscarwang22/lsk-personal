4e75d81c1a4dff4f96cb98311ffe823ae36323ba
Vincent Driessen <me@nvie.com>
2023-10-31 08:59:49 +0100

    Link to local Bun server @ examples/nextjs-live-cursors-chat

--- examples/nextjs-live-cursors-chat/liveblocks.config.ts
+++ examples/nextjs-live-cursors-chat/liveblocks.config.ts
@@ -5,6 +5,10 @@ import { createRoomContext } from "@liveblocks/react";
 const client = createClient({
   throttle: 16,
   publicApiKey: process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY!,
+
+  // @ts-expect-error
+  baseUrl: "http://127.0.0.1:3333/",
+  enableDebugLogging: true,
 });
 
 // Presence represents the properties that will exist on every User in the Room
